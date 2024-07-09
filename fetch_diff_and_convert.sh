1cdf5b0b0d6471999eb4c187a4bec83052cdf674
chungdaw <ic.chungdau.wang@gmail.com>
2024-07-09 04:59:22 +0000

    update at 2024/07/09 13:00

--- fetch_diff_and_convert.sh
+++ fetch_diff_and_convert.sh
@@ -20,10 +20,12 @@ LOCAL_FILE="${HOME}/workspace/AS-FAQ-PREVIEW/source/${CONVERTED_FILE_NAME}"
 # Download the remote file to a temporary file
 curl -L "https://drive.google.com/uc?id=${GOOGLE_FILE_ID}" -o ${TEMP_FILE}
 
+
+# Check if the download was successfully
 if [ $? -eq 0 ]; then
   iconv -f big5 -t utf-8 ${TEMP_FILE} -o ${UTF8_TEMP_FILE}
 
-  # If convert the temporary file successfully
+  # Check if the convert was successfully
   if [ $? -eq 0 ]; then
     echo "${CONVERTED_FILE_NAME} downloaded and converted to utf8 successfully."
     sed -i 's/\r//' ${UTF8_TEMP_FILE}
@@ -35,13 +37,13 @@ if [ $? -eq 0 ]; then
     else
       # Compare the temporary file and the local file
       if ! diff "${UTF8_TEMP_FILE}" "${LOCAL_FILE}" > /dev/null; then
-          # If the files are different, update the local file
-	  mv "${UTF8_TEMP_FILE}" "${LOCAL_FILE}"
-	  echo "File updated: ${LOCAL_FILE}"
+        # If the files are different, update the local file
+	mv "${UTF8_TEMP_FILE}" "${LOCAL_FILE}"
+	echo "File updated: ${LOCAL_FILE}"
       else
-	  # If the files are the same, delete the temporary file
-	  rm "${UTF8_TEMP_FILE}"
-	  echo "Files are the same, no update needed."
+	# If the files are the same, delete the temporary file
+	rm "${TEMP_FILE}" "${UTF8_TEMP_FILE}"
+	echo "Files are the same, no update needed."
       fi
     fi
   fi
